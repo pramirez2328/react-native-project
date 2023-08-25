dddcb8a8706309f10d9322e37cc5c1f7981a5d2a
ramirez <prlara2000@gmail.com>
2023-08-24 21:30:15 -0500

    gesture

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -10,10 +10,23 @@ export const fetchComments = createAsyncThunk('comments/fetchComments', async ()
   return data;
 });
 
+export const postComment = createAsyncThunk('comments/postComment', async (payload, { dispatch, getState }) => {
+  setTimeout(() => {
+    const { comments } = getState();
+    payload.date = new Date().toISOString();
+    payload.id = comments.commentsArray.length;
+    dispatch(addComment(payload));
+  }, 2000);
+});
+
 const commentsSlice = createSlice({
   name: 'comments',
   initialState: { isLoading: true, errMess: null, commentsArray: [] },
-  reducers: {},
+  reducers: {
+    addComment: (state, action) => {
+      state.commentsArray.push(action.payload);
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchComments.pending, (state) => {
@@ -32,3 +45,4 @@ const commentsSlice = createSlice({
 });
 
 export const commentsReducer = commentsSlice.reducer;
+export const { addComment } = commentsSlice.actions;
